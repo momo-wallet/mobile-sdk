9e1d14bcbfb95b6816863c1c3c05b7183e689ade
momodevelopment <lanhluu.vn@gmail.com>
2019-02-12 15:30:09 +0700

    remove InetAddressUtils, add function encryptRSA

--- android/momo_partner_sdk/src/main/java/vn/momo/momo_partner/utils/MoMoUtils.java
+++ android/momo_partner_sdk/src/main/java/vn/momo/momo_partner/utils/MoMoUtils.java
@@ -26,33 +26,32 @@ import javax.crypto.NoSuchPaddingException;
 
 public class MoMoUtils {
     public static String getIPAddress(boolean useIPv4) {
-        try {
-            ArrayList ex = Collections.list(NetworkInterface.getNetworkInterfaces());
-            Iterator i$ = ex.iterator();
-            while(i$.hasNext()) {
-                NetworkInterface intf = (NetworkInterface)i$.next();
-                ArrayList addrs = Collections.list(intf.getInetAddresses());
-                Iterator i$1 = addrs.iterator();
-                while(i$1.hasNext()) {
-                    InetAddress addr = (InetAddress)i$1.next();
-                    if(!addr.isLoopbackAddress()) {
-                        String sAddr = addr.getHostAddress().toUpperCase();
-                        boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
-                        if(useIPv4) {
-                            if(isIPv4) {
-                                return sAddr;
-                            }
-                        } else if(!isIPv4) {
-                            int delim = sAddr.indexOf(37);
-                            return delim < 0?sAddr:sAddr.substring(0, delim);
-                        }
-                    }
-                }
-            }
-        } catch (Exception var10) {
-
-        }
         return "0.0.0.0";
+    }
+    
+    public static String encryptRSA(String source, String publicKey) {
+        byte[] publicKeyByte = Base64.decode(publicKey, 2);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyByte);
+        String encrypted = "";
+        try {
+            KeyFactory e = KeyFactory.getInstance("RSA");
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher.init(1, e.generatePublic(keySpec));
+            encrypted = Base64.encodeToString(cipher.doFinal(source.getBytes()), 2);
+        } catch (NoSuchAlgorithmException var7) {
+            var7.printStackTrace();
+        } catch (NoSuchPaddingException var8) {
+            var8.printStackTrace();
+        } catch (InvalidKeySpecException var9) {
+            var9.printStackTrace();
+        } catch (InvalidKeyException var10) {
+            var10.printStackTrace();
+        } catch (BadPaddingException var11) {
+            var11.printStackTrace();
+        } catch (IllegalBlockSizeException var12) {
+            var12.printStackTrace();
+        }
+        return encrypted;
     }
 
     public static String encodeString(String s) {
